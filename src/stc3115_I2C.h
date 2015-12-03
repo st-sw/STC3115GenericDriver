c2aff210a0acd60bf6503479947c96816cc3a0a3
st-sw <st-sw@users.noreply.github.com>
2015-12-02 18:45:43 -0800

    specifiy STC3115 I2C address

--- src/stc3115_I2C.h
+++ src/stc3115_I2C.h
@@ -21,6 +21,10 @@
 #ifndef __STC3115_I2C_H
 #define __STC3115_I2C_H
 
+#define STC3115_SLAVE_ADDRESS_7BIT 0x70
+#define STC3115_SLAVE_ADDRESS_8BIT_WRITE 0xE0
+#define STC3115_SLAVE_ADDRESS_8BIT_READ  0xE1
+
 int I2C_Write(int NumberOfBytes, int RegAddress , unsigned char *TxBuffer);
 int I2C_Read (int NumberOfBytes, int RegAddress , unsigned char *RxBuffer);
 
